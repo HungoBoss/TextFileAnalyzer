a5e47e13b78cae64181f4951a55c972e788e5716
nqhun <ngoqhu@gmail.com>
2021-04-21 06:59:04 +0200

    Added resources for czech localization and current UI culture info

--- TextFileAnalyzer/TextFileAnalyzer/App.xaml.cs
+++ TextFileAnalyzer/TextFileAnalyzer/App.xaml.cs
@@ -13,5 +13,9 @@ namespace TextFileAnalyzer
     /// </summary>
     public partial class App : Application
     {
+        App() { 
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("cs");
+            //System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en"); 
+        } 
     }
 }
--- TextFileAnalyzer/TextFileAnalyzer/Properties/Resources.Designer.cs
+++ TextFileAnalyzer/TextFileAnalyzer/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace TextFileAnalyzer.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace TextFileAnalyzer.Properties {
         ///   Vrací instanci ResourceManager uloženou v mezipaměti použitou touto třídou.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TextFileAnalyzer.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace TextFileAnalyzer.Properties {
         ///   vyhledání prostředků pomocí třídy prostředků se silnými typy.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace TextFileAnalyzer.Properties {
         /// <summary>
         ///   Vyhledá lokalizovaný řetězec podobný Select a folder.
         /// </summary>
-        internal static string SelectFolderButton {
+        public static string SelectFolderButton {
             get {
                 return ResourceManager.GetString("SelectFolderButton", resourceCulture);
             }
